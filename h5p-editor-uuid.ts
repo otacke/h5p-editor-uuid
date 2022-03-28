0bfeacafcf4d91772c4a2cd46787f03d4df381c9
Sindre Bøyum <sindre.boyum@tietoevry.com>
2022-03-28 15:02:23 +0200

    remove unnecessary logs

--- h5p-editor-uuid.ts
+++ h5p-editor-uuid.ts
@@ -1,20 +1,9 @@
-import { H5PFieldText, H5PForm, H5PSetValue, IH5PWidget } from "h5p-types";
+import { H5PFieldText, IH5PWidget } from "h5p-types";
 import { H5PWidget } from "h5p-utils/src/models/H5PWidget";
 import { H5P, H5PEditor } from "h5p-utils/src/utils/H5P.utils";
 
 class UUIDWidget extends H5PWidget<H5PFieldText> implements IH5PWidget {
-  constructor(
-    parent: H5PForm<string>,
-    field: H5PFieldText,
-    params: string | undefined,
-    setValue: H5PSetValue<string>
-  ) {
-    super(parent, field, params, setValue);
-    console.log("constructing uuid widget");
-  }
-
   appendTo() {
-    console.log("appending uuid widget");
     const { field, params, setValue } = this;
 
     const isTextField = field.type === "text";
