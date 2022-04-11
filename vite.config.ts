2ce879d8b7f84051f8a476737d8b600f176aceea
Sindre Bøyum <sindre.boyum@tietoevry.com>
2022-04-11 19:39:47 +0200

    scope widget in IIFE

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,30 @@
-import { defineConfig } from "vite";
+import type { OutputAsset, OutputChunk } from "rollup";
+import { defineConfig, PluginOption } from "vite";
+
+const isOutputChunk = (
+  chunkOrAsset: OutputChunk | OutputAsset
+): chunkOrAsset is OutputChunk => {
+  return chunkOrAsset["code"] != null;
+};
+
+const wrapIIFE = (): PluginOption => ({
+  name: "wrap-iife",
+  generateBundle(options, bundle) {
+    const chunks = Object.values(bundle);
+
+    for (let i = 0; i < chunks.length; i += 1) {
+      const chunk = chunks[i];
+      if (isOutputChunk(chunk)) {
+        // eslint-disable-next-line no-param-reassign
+        chunk.code = `(function(){\n${chunk.code}\n})()`;
+      }
+    }
+  },
+});
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [],
+  plugins: [wrapIIFE()],
   build: {
     minify: "esbuild",
 
